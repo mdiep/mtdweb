cf9951536eafb0405c2791c0b04046346a070975
Karl Meisterheim <kmeister@karl-meisterheims-computer.local>
2008-05-05 16:44:51 -0400

    Added edit function for notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -9,4 +9,20 @@ class NotesController < ApplicationController
         
         redirect_to @note.contact
     end
+    
+    def edit
+        @note = Note.find(params[:id])        
+    end
+    
+    def update
+        @note = Note.find(params[:id])
+      
+        if @note.update_attributes(params[:note])
+            redirect_to @note.contact
+        else
+            flash[:error] = "Unable to update note"
+            render :action => 'edit'
+        end
+    end
+
 end
