a46c5bcedde1821b0ca283cdc2965e01ee9a2100
Matt Diephouse <matt@diephouse.com>
2008-05-13 12:26:45 -0400

    Add a "Add Phone Number" link

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -4,4 +4,10 @@ module ContactsHelper
         prefix = pn.new_record? ? 'new' : 'existing'
         fields_for("contact[#{prefix}_phone_number_attributes][]", pn, &block)
     end
+    
+    def add_partial_link(name, id, partial, object)
+        link_to_function name, nil, :class => 'add-partial' do |page|
+            page.insert_html :bottom, id, :partial => partial, :object => object
+        end
+    end
 end
