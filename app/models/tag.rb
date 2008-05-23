b09015856105ae9d6e5aea45379f50c2a76c7159
Matt Diephouse <matt@diephouse.com>
2008-05-22 21:44:12 -0400

    Major UI overhaul

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,8 @@ class Tag < ActiveRecord::Base
     belongs_to :user
     has_many :taggings, :dependent => :destroy
     has_many :contacts, :through => :taggings
+    
+    def <=>(right)
+        return self.name <=> right.name
+    end
 end
